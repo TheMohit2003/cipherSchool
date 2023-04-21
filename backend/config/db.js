17cee94332b74fa2fd3eb3b0a46f0af8d2719f70
mohit <mohitpardeshi729@gmail.com>
2023-04-21 12:45:00 +0530

    connecting to database through mongoose

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+/* eslint-env node */
+const mongoose = require("mongoose");
+require("dotenv").config();
+const db = process.env.DB_URI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db);
+    console.log("Mongodb Connected");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;

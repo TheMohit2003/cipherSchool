5569d448b14a211f971232924e6eb653861c9ad2
mohit <mohitpardeshi729@gmail.com>
2023-04-21 12:43:07 +0530

    added requried middleware and route responder

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 /* eslint-env node */
 const express = require("express");
-const app = express();
+const connectDB = require("./config/db");
+connectDB();
 
+const app = express();
+const cors = require("cors");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
+
+app.use("/", require("./routes/user"));
 
 app.listen(PORT, () => {
   console.log(`the port is running on http://localhost:${PORT}/`);

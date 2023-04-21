225062217b1db7d36f08c40ea46af26f454fa083
mohit <mohitpardeshi729@gmail.com>
2023-04-21 10:44:00 +0530

    basic file stucutre completed

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`the port is running on http://localhost:${PORT}/`);
+});
